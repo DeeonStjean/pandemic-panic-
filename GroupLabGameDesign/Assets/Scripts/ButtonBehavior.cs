f3126487d7cf616333a21d686c7ddf42c84fb65c
DeeonStjean <deeonstjean101@gmail.com>
2023-05-21 20:58:12 -0400

    add scenes to build

--- GroupLabGameDesign/Assets/Scripts/ButtonBehavior.cs
+++ GroupLabGameDesign/Assets/Scripts/ButtonBehavior.cs
@@ -27,6 +27,31 @@ public class ButtonBehavior : MonoBehaviour
         //AudioSource.PlayClipAtPoint(clickSoundEffect, transform.position);
         SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex) + 1);
     }
+    public void LoadM1NewsScene1()
+    {
+        SceneManager.LoadScene("m1NewsSegment");
+    }
+    public void LoadM1NewsScene2()
+    {
+        SceneManager.LoadScene("m1NewsSegment#2");
+    }
+
+    public void LoadM2NewsScene1()
+    {
+        SceneManager.LoadScene("m2NewsSegment");
+    }
+    public void LoadM2NewsScene2()
+    {
+        SceneManager.LoadScene("m2NewsSegment#2");
+    }
+
+    public void loadNextDes()
+    {
+        SceneManager.LoadScene("m2week1des1");
+    }
+    public void loadNextDes2() {
+        SceneManager.LoadScene("m3week1des1");
+    }
 
     // Decision reporting
     public void report()
